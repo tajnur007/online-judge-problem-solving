72ad3ae11d0115440f8b183f347c80d013060191
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-12-28 13:47:03 +0600

    Codeforces 102267A solution addded

---
+++ codeforces/102267A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x, y;
+    cin >> x >> y;
+    if(x<y) cout << "0\n";
+    else cout << "1\n";
+
+
+    return 0;
+}
