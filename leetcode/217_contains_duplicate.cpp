be9d3f32270db5d9eabe7ea071b2bf746909b723
tajnur007 <tajnur007@gmail.com>
2022-01-01 23:11:31 +0600

    Leetcode 217_contains_duplicate solution added

---
+++ leetcode/217_contains_duplicate.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int len = nums.size() - 1;
+        
+        for(int i=0; i<len; i++) {
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        
+        return false;
+    }
+};
